7fef2847c7276f6e5f5d8292a48f97cad0638f40
sergiorodas <sergiorodasbj@hotmail.com>
2021-09-09 11:57:22 -0300

    Configura nuevo email

--- js/contact.js
+++ js/contact.js
@@ -1,10 +1,7 @@
 $(function () {
-
     "use strict";
 
     // init the validator
-    // validator files are included in the download package
-    // otherwise download from http://1000hz.github.io/bootstrap-validator
 
     $('#contact-form').validator();
 
@@ -13,35 +10,42 @@ $(function () {
     $('#contact-form').on('submit', function (e) {
 
         // if the validator does not prevent form submit
-        // if (!e.isDefaultPrevented()) {
-        //     var url = "https://formsubmit.co/sergiorodasgonzalez@gmail.com";
+        if (!e.isDefaultPrevented()) {
+            var url = "https://formsubmit.co/ajax/contact@sergiorodas.com";
 
-        //     // POST values in the background the the script URL
-        //     $.ajax({
-        //         type: "POST",
-        //         url: url,
-        //         data: $(this).serialize(),
-        //         success: function (data)
-        //         {
-        //             // data = JSON object that contact.php returns
+            // POST values in the background the the script URL
+            $.ajax({
+                url: url,
+                method: "POST",
+                data: $(this).serialize(),
+                dataType: "json",
+                success: function (data)
+                {
 
-        //             // we recieve the type of the message: success x danger and apply it to the 
-        //             var messageAlert = 'alert-' + data.type;
-        //             var messageText = data.message;
+                    // we recieve the type of the message: success x danger and apply it to the 
+                    var messageAlert = 'alert-' + data.type;
 
-        //             // let's compose Bootstrap alert box HTML
-        //             var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    if(document.querySelector(".contact-button-2").lastChild.textContent == "Enviar Mensaje") {
+                        var messageText = "El formulario ha sido enviado exitosamente.";
+                    } else {
+                        var messageText = data.message;
+                    }
+
                     
-        //             // If we have messageAlert and messageText
-        //             if (messageAlert && messageText) {
-        //                 // inject the alert to .messages div in our form
-        //                 $('#contact-form').find('.messages').html(alertBox);
-        //                 // empty the form
-        //                 $('#contact-form')[0].reset();
-        //             }
-        //         }
-        //     });
-        //     return false;
-        // }
+
+                    // let's compose Bootstrap alert box HTML
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    
+                    // If we have messageAlert and messageText
+                    if (messageAlert && messageText) {
+                        // inject the alert to .messages div in our form
+                        $('#contact-form').find('.messages').html(alertBox);
+                        // empty the form
+                        $('#contact-form')[0].reset();
+                    }
+                }
+            });
+            return false;
+        }
     })
 });
